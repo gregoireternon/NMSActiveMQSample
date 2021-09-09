099fa69458cb650c84c8da33c6035a0484bbf4b5
gternon@groupedigit.com <gternon@groupedigit.com>
2021-09-09 17:56:08 +0200

    Initial commit

---
+++ JMSConsumer1/JMSConsumer1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+using System;
+
+namespace JMSConsumer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string clientId = "customer1";
+
+            IConnectionFactory factory = new    ConnectionFactory("tcp://localhost:61616/", clientId);
+
+   //Create the connection
+
+            using (IConnection connection =  factory.CreateConnection())
+
+            {
+
+
+                connection.Start();
+
+                //Create the Session
+
+                using (ISession session = connection.CreateSession())
+
+                {
+
+                    //Create the Consumer
+
+                    IMessageConsumer consumer = session.CreateDurableConsumer(new ActiveMQTopic("myTopic"),clientId, null,false);
+
+                    consumer.Listener += new MessageListener(
+
+                       consumer_Listener);
+
+                    Console.ReadLine();
+
+                }
+
+            }
+
+        }
+
+        static void consumer_Listener(IMessage message)
+
+        {
+
+            Console.WriteLine("Receive: " +     ((ITextMessage)message).Text);
+
+        }
+
+    }
+}
---
+++ JMSProducer/JMSProducer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+using System;
+using System.Threading;
+
+namespace JMSProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string ip = "tcp://localhost:61616";
+            Uri providerUri = new Uri(ip);
+            IConnection conn = null;
+            IConnectionFactory factory = new ConnectionFactory(ip);
+            using (conn = factory.CreateConnection())
+            {
+                using(ISession ses = conn.CreateSession())
+                {
+                    
+
+                    IMessageProducer prod = ses.CreateProducer(new ActiveMQTopic("myTopic"));
+
+                    while (true)
+                    {
+                        Thread.Sleep(2000);
+                        string message = "coucou" + DateTime.Now.ToString();
+                        Console.WriteLine("Sending message : " + message);
+                        IMessage myMessage = prod.CreateTextMessage(message);
+                        prod.Send(myMessage,MsgDeliveryMode.Persistent, MsgPriority.Normal,TimeSpan.FromSeconds(3600));
+
+                    }
+
+                }
+            }
+                
+        }
+    }
+}
